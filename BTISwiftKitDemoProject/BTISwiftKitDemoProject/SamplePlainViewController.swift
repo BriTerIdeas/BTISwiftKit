de3299c74da1033bf7c042dc09cdeb2c3b4538d2
Brian Slick <contact@briterideas.com>
2015-09-24 09:46:39 -0400

    - Preliminary demo app

---
+++ BTISwiftKitDemoProject/BTISwiftKitDemoProject/SamplePlainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SamplePlainViewController.swift
+//  BTISwiftKitDemoProject
+//
+//  Created by Brian Slick on 9/24/15.
+//  Copyright © 2015 BriTer Ideas LLC. All rights reserved.
+//
+
+import Foundation
+
+class SampleViewController: StandardViewController
+{
+    
+}
---
+++ BTISwiftKitDemoProject/BTISwiftKitDemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTISwiftKitDemoProject
+//
+//  Created by Brian Slick on 9/24/15.
+//  Copyright © 2015 BriTer Ideas LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
